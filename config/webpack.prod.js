ff17b2f9072da69cc10f4ac7c5020ba411f05ff4
Yuriy Bezgachnyuk <yurkovskiy@gmail.com>
2016-10-18 09:40:50 +0300

    Added prestart task into package.json. Modified config files for WebPack: changed name of dist directories

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = webpackMerge(commonConfig, {
 
     output: {
         path: helpers.root('dist'),
-        publicPath: 'http://dtapi.local/~pupkin/dt069/dist/',
+        publicPath: 'http://dtapi.local/~pupkin/DTester/dist/',
         filename: '[name].[hash].js',
         chunkFilename: '[id].[hash].chunk.js'
     },
