869b1dc2a796137a8eb541c36b122888bc961849
ivan.fetsenets@gmail.com <yurkovskiy@gmail.com>
2016-10-25 00:19:40 +0300

    student component corrected

--- src/app/shared/services/student.service.ts
+++ src/app/shared/services/student.service.ts
@@ -24,15 +24,14 @@ export class SubjectService {
         return Observable.throw(errMsg);
     }
 
-    private handleError = (error:any)=> {
+    private handleError(error: any) {
+        // In a real world app, we might use a remote logging infrastructure
+        // We'd also dig deeper into the error to get a better message
         let errMsg = (error.message) ? error.message :
             error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-        if (error.status == "403") {
-            sessionStorage.removeItem("userRole");
-            this.router.navigate(['/login'])
-        }
+        console.error(errMsg); // log to console instead
         return Observable.throw(errMsg);
-    };
+    }
 
     public getSubjects(): Observable<Subject[]> {
         return this.http
