630b454f21dc841f10c94e41fc6fd56b50eaf4c6
Volodymyr Ilemskyi <v.ilemsky@gmail.com>
2016-10-25 10:39:15 +0300

    added background to login page; added resolve-url loader to webpack config

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -39,7 +39,7 @@ module.exports = {
             {
                 test: /\.css$/,
                 include: helpers.root('src', 'app'),
-                loader: 'raw'
+                loader: 'raw!resolve-url'
             }
         ]
     },
