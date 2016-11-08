327b025b5b1ff90f8ed90d9fd4c3f8192541de85
Volodymyr Ilemskyi <v.ilemsky@gmail.com>
2016-11-08 21:06:17 +0200

    now test-player calculate result of test

--- src/app/test-player/test-player.component.ts
+++ src/app/test-player/test-player.component.ts
@@ -11,24 +11,16 @@ import {TestPlayerService} from "../shared/services/test-player.service";
 
 export class TestPlayerComponent implements OnInit {
 
-    private test: any = {
-        test_id: 3,
-        test_name: "Система контролю версій",
-        subject_id: 1,
-        tasks: 5,
-        time_for_test: 10,
-        enabled: 1,
-        attempts: 1
-    };
+    private testId: number = 3;
 
-    private navButtons: any[] = [
-        {answered: false, name: "01", active: true, className: "btn btn-warning nom-qua"}];
+    private navButtons: any[];
 
     private activeQuestion: number = 1;
     private questions: any[] = [];
+    private tasksCount: number = 0;
     private questionCount: number = 0;
-    private testDetails: any;
     private show: boolean = false;
+    private testDetails: any[];
 
     constructor(private router: Router,
                 private subjectService: SubjectService,
@@ -36,36 +28,38 @@ export class TestPlayerComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.subjectService.getTestDetailsByTest(this.test.test_id)
+        this.subjectService.getTestDetailsByTest(this.testId)
             .subscribe(testDetails => {
                 this.testDetails = testDetails;
+                testDetails.forEach((data) => {
+                    this.tasksCount += +data.tasks;
+                });
                 testDetails.forEach((item) => {
                     this.subjectService.getQuestionsByLevelRand(item.test_id, item.level, item.tasks)
                         .subscribe(response => {
                             this.questionCount += +item.tasks;
                             response.forEach(question => {
+                                question.chosenAnswer = {};
+                                question.rate = item.rate;
                                 this.questions.push(question);
                             });
-                            this.questions.sort((a, b) => {
-                                return Math.random() > 0.5 ? +a.question_id - +b.question_id : +b.question_id - +a.question_id;
-                            });
-                            if (this.questionCount === this.test.tasks) {
-                                for (let i = 1; i < this.questions.length; i++) {
-                                    this.navButtons.push({});
-                                    this.navButtons[i].answered = false;
-                                    this.navButtons[i].name = i + 1 < 10 ? `0${i + 1}` : i + 1;
-                                    this.navButtons[i].className = "btn btn-primary nom-qua";
-                                    this.navButtons[i].active = false;
-                                }
-                                this.questions.forEach((elem) => {
+                            if (this.questionCount === this.tasksCount) {
+                                this.questions.sort((a, b) => {
+                                    return Math.random() > 0.5 ? +a.question_id - +b.question_id : +b.question_id - +a.question_id;
+                                });
+                                this.navButtons = this.testPlayerService.createButtons(this.questions.length);
+                                this.questions.forEach((elem, j) => {
                                     this.testPlayerService.getAnswersByQuestion(elem.question_id)
                                         .subscribe(answers => {
+                                            answers.sort((a, b) => {
+                                                return Math.random() > 0.5 ? +a.answer_id - +b.answer_id : +b.answer_id - +a.answer_id;
+                                            });
                                             elem.answers = answers;
-                                            console.log("full question ", elem.question_id + " : " + JSON.stringify(elem, null, 2));
+                                            if (j === this.questions.length - 1) {
+                                                this.show = true;
+                                            }
                                         });
                                 });
-                                this.show = true;
-                                // console.log(JSON.stringify(this.questions, null, 2));
                             }
                         });
                 });
@@ -80,9 +74,16 @@ export class TestPlayerComponent implements OnInit {
 
     answerQuestion() {
         this.navButtons[this.activeQuestion - 1].answered = true;
-        this.activeQuestion === this.questions.length ?
-            console.log("Питання закінчились") :
+        if (this.activeQuestion === this.questions.length) {
+            this.testPlayerService.checkSAnswers(this.questions)
+                .subscribe(results => {
+                    let userRate = this.testPlayerService.getUserRate(results, this.questions);
+                    alert("your rate is: " + userRate);
+                });
+        }
+        else {
             this.changeActiveQuestion(this.activeQuestion + 1);
+        }
     };
 
     changeActiveQuestion(num: number) {
@@ -93,4 +94,10 @@ export class TestPlayerComponent implements OnInit {
         this.activeQuestion = num;
         this.navButtons[this.activeQuestion - 1].className = "btn btn-warning nom-qua";
     }
+
+    toggleAnswer(event: any, answerId: number, numberOfQuestion: number) {
+        this.questions[numberOfQuestion].chosenAnswer[answerId] = !this.questions[numberOfQuestion].chosenAnswer[answerId];
+        event.stopImmediatePropagation();
+    }
+
 }
