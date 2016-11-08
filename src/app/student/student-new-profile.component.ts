8818eab771eb08e38efdc8166a11ee0024cc3006
Ivan <ivan.fetsenets@gmail.com>
2016-11-08 10:58:02 +0200

    commit before pushing

--- src/app/student/student-new-profile.component.ts
+++ src/app/student/student-new-profile.component.ts
@@ -17,7 +17,7 @@ import {
 @Component({
     templateUrl: "student-new-profile.component.html",
     styleUrls: ["student.component.css"],
-   // directives: [ImageActionsComponent]
+    // directives: [ImageActionsComponent]
 })
 export class StudentNewProfileComponent implements OnInit {
 
@@ -39,13 +39,12 @@ export class StudentNewProfileComponent implements OnInit {
     public statusView: boolean = false;
     public editSaveButtonStatus: boolean = false;
     public modalInfoConfig: any = modalInfoConfig;
-    public file_srcs: string[] = [];
+    public foto_src: string = "";
 
     @ViewChild("inputFoto") inputFoto;
     @ViewChild("inputFotoStud") inputFotoStud;
 
-    constructor(
-                private _commonService: CRUDService,
+    constructor(private _commonService: CRUDService,
                 private location: Location,
                 private modalService: NgbModal,
                 private element: ElementRef) {
@@ -103,39 +102,42 @@ export class StudentNewProfileComponent implements OnInit {
 
     getGroupName(id: number) {
         this.idFaculty = id;
+        this.groups = [];
         this._commonService.getGroupsByFaculty(this.idFaculty)
             .subscribe(data => {
-                this.groups = data;
+                    if (data.response === "no records") {
+                        this.groups.push(new Group("Для даного факультету не зареэстровано жодноъ групи"));
+                    } else {this.groups = data; }
                 },
                 error => console.log("error: ", error)
             );
     }
 
     createNewStudent() {
-                this._commonService.getGroupsByFaculty(this.idFaculty)
-                    .subscribe(data => {
-                            this.groups = data;
-                            this.newStudent[0].username = this.student[0].username;
-                            this.newStudent[0].password = this.student[0].plain_password;
-                            this.newStudent[0].password_confirm = this.student[0].plain_password;
-                            this.newStudent[0].email = this.student[0].email;
-                            this.newStudent[0].gradebook_id = this.student[0].gradebook_id;
-                            this.newStudent[0].student_surname = this.student[0].student_surname;
-                            this.newStudent[0].student_name = this.student[0].student_name;
-                            this.newStudent[0].student_fname = this.student[0].student_fname;
-                            this.newStudent[0].group_id = this.student[0].group_id;
-                            this.newStudent[0].plain_password = this.student[0].plain_password;
-                        console.log("this.inputFoto : ", this.inputFotoStud.src);
-                            this.newStudent[0].photo = this.inputFoto;
-                            this._commonService.insertData(this.entity, this.newStudent[0])
-                                .subscribe(data => {
-                                        console.log("data : ", data);
-                                    },
-                                    error => console.log("error: ", error)
-                                );
-                        },
-                        error => console.log("error: ", error)
-                    );
+        this._commonService.getGroupsByFaculty(this.idFaculty)
+            .subscribe(data => {
+                    this.groups = data;
+                    this.newStudent[0].username = this.student[0].username;
+                    this.newStudent[0].password = this.student[0].plain_password;
+                    this.newStudent[0].password_confirm = this.student[0].plain_password;
+                    this.newStudent[0].email = this.student[0].email;
+                    this.newStudent[0].gradebook_id = this.student[0].gradebook_id;
+                    this.newStudent[0].student_surname = this.student[0].student_surname;
+                    this.newStudent[0].student_name = this.student[0].student_name;
+                    this.newStudent[0].student_fname = this.student[0].student_fname;
+                    this.newStudent[0].group_id = this.student[0].group_id;
+                    this.newStudent[0].plain_password = this.student[0].plain_password;
+                    console.log("this.inputFoto : ", this.inputFotoStud.src);
+                    this.newStudent[0].photo = this.inputFoto;
+                    this._commonService.insertData(this.entity, this.newStudent[0])
+                        .subscribe(data => {
+                                console.log("data : ", data);
+                            },
+                            error => console.log("error: ", error)
+                        );
+                },
+                error => console.log("error: ", error)
+            );
     }
 
     activate(data: any) {
@@ -154,53 +156,54 @@ export class StudentNewProfileComponent implements OnInit {
         const modalRefDel = this.modalService.open(InfoModalComponent, {size: "sm"});
         modalRefDel.componentInstance.config = this.modalInfoConfig;
         /*modalRefDel.result
-            .then(() => {
-                this.delRecord(this.entity, data.entity_id);
-            }, () => {
-                return;
-            })*/
+         .then(() => {
+         this.delRecord(this.entity, data.entity_id);
+         }, () => {
+         return;
+         })*/
     }
 
-   /* fileChange(input) {
-            let input = event.target;
+    /* fileChange(input) {
+     let input = event.target;
 
-            let reader = new FileReader();
-            reader.onload = function(){
-                var dataURL = reader.result;
-                var output = document.getElementById("output");
-                output.src = dataURL;
-            };
-            reader.readAsDataURL(input.files[0]);*/
+     let reader = new FileReader();
+     reader.onload = function(){
+     var dataURL = reader.result;
+     var output = document.getElementById("output");
+     output.src = dataURL;
+     };
+     reader.readAsDataURL(input.files[0]);*/
 
 
-        /*changeListner(event) {
-            let reader = new FileReader();
-            let image = this.element.nativeElement.querySelector("#output");
+    /*changeListner(event) {
+     let reader = new FileReader();
+     let image = this.element.nativeElement.querySelector("#output");
 
-            reader.onload = function(e) {
-                image.src = e.target.result;
-            };
+     reader.onload = function(e) {
+     image.src = e.target.result;
+     };
 
-            reader.readAsDataURL(event.target.files[0]);
-        }*/
+     reader.readAsDataURL(event.target.files[0]);
+     }*/
 
-    openFile (event) {
-    let input = event.target;
+    openFile(event) {
+        let input = event.target;
 
-    let reader = new FileReader();
-    reader.onload = function(){
-        // let dataURL = reader.result;
-        // let output = document.getElementById("output");
-        // var dataURL = reader.result;
-        // var output = document.getElementById('output');
-        // output.src = dataURL;
-        let dataURL = reader.result;
-        // console.log("dataURL : ", dataURL);
-        // this.inputFotoStud.src = dataURL;
-    };
-    reader.readAsDataURL(input.files[0]);
-    this.inputFotoStud.src = reader.readAsDataURL(input.files[0]);
-        console.log("reader.readAsDataURL(input.files[0]): ", reader.readAsDataURL(input.files[0]));
-};
+        let reader = new FileReader();
+        reader.onload = function () {
+            // let dataURL = reader.result;
+            // let output = document.getElementById("output");
+            // output.src = dataURL;
+            let dataURL = reader.result;
+            // document.getElementById("output") = dataURL;
+            console.log("dataURL : ", dataURL);
+            // this.input.src = dataURL;
+            // this.foto_src = dataURL;
+            // this.inputFotoStud.src = dataURL;
+            // console.log("this.input.src : ", this.input.src);
+        };
+        reader.readAsDataURL(input.files[0]);
+        // console.log("reader.readAsDataURL(input.files[0]): ", reader.readAsDataURL(input.files[0]));
+    }
 
 }
