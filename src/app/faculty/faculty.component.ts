2bcf27fb43dfd5bbf213a152e7a7cd2523e2c9b6
Volodymyr Ilemskyi <v.ilemsky@gmail.com>
2016-11-09 17:25:25 +0200

    test-player was upgraded: user rate now will shown in modal window, 'skip' and 'answer' buttons now have better behavior

--- src/app/faculty/faculty.component.ts
+++ src/app/faculty/faculty.component.ts
@@ -140,7 +140,7 @@ export class FacultyComponent implements OnInit {
     }
 
     deleteCase(data: any) {
-        this.modalInfoConfig.infoString = `Ви дійсно хочете видати ${data.entityColumns[1]}?`;
+        this.modalInfoConfig.infoString = `Ви дійсно хочете видалити ${data.entityColumns[1]}?`;
         this.modalInfoConfig.action = "confirm";
         this.modalInfoConfig.title = "Видалення";
         const modalRefDel = this.modalService.open(InfoModalComponent, {size: "sm"});
--- src/app/test-player/test-player.component.ts
+++ src/app/test-player/test-player.component.ts
@@ -1,39 +1,47 @@
 import {Component, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {SubjectService} from "../shared/services/subject.service";
 import {TestPlayerService} from "../shared/services/test-player.service";
-
+import {InfoModalComponent} from "../shared/components/info-modal/info-modal.component";
+import {modalInfoConfig} from "../shared/constant";
 
 @Component({
     templateUrl: "test-player.component.html",
-    styleUrls: ["../studentpart/start-page.component.css", "test-player.component.css"]
+    styleUrls: ["test-player.component.css"]
 })
 
 export class TestPlayerComponent implements OnInit {
 
+    private modalInfoConfig = modalInfoConfig;
     private testId: number = 3;
 
     private navButtons: any[];
 
-    private activeQuestion: number = 1;
+    private activeQuestion: number = 0;
     private questions: any[] = [];
     private tasksCount: number = 0;
     private questionCount: number = 0;
     private show: boolean = false;
     private testDetails: any[];
+    private maxUserRate: number = 0;
 
     constructor(private router: Router,
                 private subjectService: SubjectService,
-                private testPlayerService: TestPlayerService) {
+                private testPlayerService: TestPlayerService,
+                private modalService: NgbModal) {
     }
 
     ngOnInit() {
         this.subjectService.getTestDetailsByTest(this.testId)
             .subscribe(testDetails => {
+                console.log(JSON.stringify(testDetails, null, 2));
                 this.testDetails = testDetails;
                 testDetails.forEach((data) => {
                     this.tasksCount += +data.tasks;
+                    this.maxUserRate += +data.tasks * +data.rate;
                 });
+                console.log(this.maxUserRate);
                 testDetails.forEach((item) => {
                     this.subjectService.getQuestionsByLevelRand(item.test_id, item.level, item.tasks)
                         .subscribe(response => {
@@ -67,32 +75,54 @@ export class TestPlayerComponent implements OnInit {
     };
 
     skipQuestion(numberOfQuestion: number) {
-        numberOfQuestion > this.questions.length ?
-            console.log("Питання закінчились") :
-            this.changeActiveQuestion(this.activeQuestion + 1);
+        if (numberOfQuestion === this.questions.length) {
+            for (let i = 0; i < this.navButtons.length; i++) {
+                if (this.navButtons[i].answered === false) {
+                    this.changeActiveQuestion(i);
+                    break;
+                }
+            }
+        }
+        else this.changeActiveQuestion(this.activeQuestion + 1);
     };
 
     answerQuestion() {
-        this.navButtons[this.activeQuestion - 1].answered = true;
-        if (this.activeQuestion === this.questions.length) {
+        this.navButtons[this.activeQuestion].answered = true;
+        let finishTest = this.navButtons.some((question) => {
+            return question.answered === false;
+        });
+        if (!finishTest) {
             this.testPlayerService.checkSAnswers(this.questions)
                 .subscribe(results => {
                     let userRate = this.testPlayerService.getUserRate(results, this.questions);
-                    alert("your rate is: " + userRate);
+                    this.modalInfoConfig.title = "Результат тестування!";
+                    this.modalInfoConfig.action = "info";
+                    this.modalInfoConfig.infoString = `Кількість набраних Вами балів становить: ${userRate} з ${this.maxUserRate} максимально можливих`;
+                    let modalRef = this.modalService.open(InfoModalComponent, {size: "sm"});
+                    modalRef.componentInstance.config = this.modalInfoConfig;
                 });
         }
         else {
-            this.changeActiveQuestion(this.activeQuestion + 1);
+            if (this.activeQuestion === this.questions.length - 1) {
+                for (let i = 0; i < this.navButtons.length; i++) {
+                    if (this.navButtons[i].answered === false) {
+                        this.changeActiveQuestion(i);
+                        break;
+                    }
+                }
+            } else {
+                this.changeActiveQuestion(this.activeQuestion + 1);
+            }
         }
     };
 
     changeActiveQuestion(num: number) {
         if (num === this.activeQuestion) return;
-        this.navButtons[this.activeQuestion - 1].answered ?
-            this.navButtons[this.activeQuestion - 1].className = "btn btn-success nom-qua" :
-            this.navButtons[this.activeQuestion - 1].className = "btn btn-primary nom-qua";
+        this.navButtons[this.activeQuestion].answered ?
+            this.navButtons[this.activeQuestion].className = "btn btn-success nom-qua" :
+            this.navButtons[this.activeQuestion].className = "btn btn-primary nom-qua";
         this.activeQuestion = num;
-        this.navButtons[this.activeQuestion - 1].className = "btn btn-warning nom-qua";
+        this.navButtons[this.activeQuestion].className = "btn btn-warning nom-qua";
     }
 
     toggleAnswer(event: any, answerId: number, numberOfQuestion: number) {
