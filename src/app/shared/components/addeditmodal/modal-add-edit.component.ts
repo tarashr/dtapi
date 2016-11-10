283501734e9a5f3571d67df91b403120743b07ba
Petro Ivankiv <petro.ivankiv88@gmail.com>
2016-11-10 14:53:38 +0200

    fix some bug

--- src/app/shared/components/addeditmodal/modal-add-edit.component.ts
+++ src/app/shared/components/addeditmodal/modal-add-edit.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from "@angular/core";
-import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+import {NgbActiveModal, NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {successEventModal, modalInfoConfig} from "../../../shared/constant";
 
 @Component({
     selector: "modal-add-edit",
@@ -9,14 +10,22 @@ import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 export class ModalAddEditComponent {
 
     @Input() config: any;
+    public maxSizeOfPictures: number = 1000000;
+    public modalInfoConfig: any = modalInfoConfig;
+    public successEventModal: any = successEventModal;
 
-    constructor(private activeModal: NgbActiveModal) {
+    constructor(private activeModal: NgbActiveModal,
+                private modalService: NgbModal) {
     }
 
     openFile($event) {
         let input = $event.target;
+        if (input.files[0].size > this.maxSizeOfPictures) {
+            this.modalInfoConfig.infoString = "Перевищено максимальний розміо зображення";
+            this.successEventModal();
+        }
         let reader = new FileReader();
-        reader.onload =  () => {
+        reader.onload = () => {
             let dataURL = reader.result;
             this.config.img[0].value = dataURL;
         };
