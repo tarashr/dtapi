b62d92ad9de5c5ccba7e858b9276ea98ce723e46
Volodymyr Ilemskyi <v.ilemsky@gmail.com>
2016-11-15 13:19:57 +0200

    Added checkimg for attempts of test. Results of tests will save in server now. For timer checking app use time of server

--- src/app/shared/constant/test-player.constant.ts
+++ src/app/shared/constant/test-player.constant.ts
@@ -1,9 +1,10 @@
-import {baseUrl} from "./common-constants";
+import {baseUrl} from "./index";
 export const getAnswersByQuestionTestPlayerUrl: string = baseUrl + "SAnswer/getAnswersByQuestion/";
 export const checkSAnswerUrl: string = baseUrl + "SAnswer/checkAnswers";
-export const navButtonConstClassName: string = "btn nom-qua";
 export const getTimeStampUrl: string = baseUrl + "TestPlayer/getTimeStamp";
 export const saveEndTimeUrl: string = baseUrl + "TestPlayer/saveEndTime";
 export const getEndTimeUrl: string = baseUrl + "TestPlayer/getEndTime";
 export const resetSessionDataUrl: string = baseUrl + "TestPlayer/resetSessionData";
 export const getTestRecordUrl: string = baseUrl + "test/getRecords/";
+export const countTestPassesByStudentUrl: string = baseUrl + "Result/countTestPassesByStudent/";
+export const navButtonConstClassName: string = "btn nom-qua";
